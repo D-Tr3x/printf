3a55f9e8c28b5ff8af96fe05b7959d68b96eb824
D-Tr3x <owosenidaniel13@mail.com>
2024-08-03 09:55:22 -0700

    Created: print_digit.c

--- _printf.c
+++ _printf.c
@@ -34,6 +34,10 @@ int _printf(const char *format, ...)
 				count += print_string(args);
 			else if (*p == '%')
 				count += print_percent(args);
+			else if (*p == 'd')
+				count += print_digit;
+			else if (*p == 'i')
+				count += print_int;
 			else /* For error statements */
 			{
 				count += _putchar('%');
---
+++ print_digit.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_digit - prints a digit or decimal
+ * @args: argument list
+ *
+ * Return: number of characters to print
+ */
+int print_digit(va_list args)
+{
+	int count = 0;
+	int num, decimal;
+	num = va_arg(args);
+
+	if (num == 0)
+		return (_putchar('0'));
+	if (num < 0)
+	{
+		count = _putchar('-');
+		num = -num;
+	}
+	while (num > 0)
+	{
+		decimal = num % 10;
+		_putchar(decimal + '0');
+		count++;
+		num /= 10;
+	}
+	
+	
+	return (count);
+}
